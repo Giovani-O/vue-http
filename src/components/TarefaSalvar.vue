144aea06c69a9ec397c03b6f6453d4f165d64b6a
Giovani O <giovani.o@outlook.com.br>
2021-09-13 11:09:07 -0300

    feat(tasks): Inicia o projeto

---
+++ src/components/TarefaSalvar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="mt-4">
+    <hr />
+    <h2 class="font-weight-light">Salvar Tarefa</h2>
+    <form>
+      <div class="row">
+        <div :class="classeColuna">
+          <div class="form-group">
+            <label>Título</label>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Título da tarefa"
+            />
+          </div>
+        </div>
+        <div class="col-sm-2" v-if="tarefa">
+          <div class="form-group">
+            <label>Tarefa concluída?</label>
+            <button class="btn btn-secondary btn-sm d-block">
+              <i class="fa fa-check"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tarefa: {
+      type: Object,
+      default: undefined,
+    },
+  },
+  data() {
+    return {
+      tarefaLocal: Object.assign(
+        {},
+        { titulo: "", concluido: false },
+        this.tarefa
+      ),
+    };
+  },
+  computed: {
+    classeColuna() {
+      return this.tarefa 
+				? "col-sm-10" 
+				: "col-sm-12";
+    },
+  },
+};
+</script>
+
+<style scoped>
+	.btn-primary {
+		background-color: rgb(108, 39, 187) !important;
+		border-color: rgb(83, 30, 143) !important;
+	}
+</style>
