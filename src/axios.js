7e1e8121fbd459007105104992ee398f85e290ed
Giovani O <giovani.o@outlook.com.br>
2021-09-20 11:01:35 -0300

    refactor(tasks): implementa promise na instância do Axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,27 @@
 import axios from 'axios'
 import config from './config/config'
 
-axios.defaults.baseURL = config.apiURL
+const instance = axios.create({})
 
-export default axios
+instance.defaults.baseURL = config.apiURL
+
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisição: ', config)
+    config.data = {
+        ...config.data,
+        curso: 'VueJS'
+    }
+    // return config
+    return new Promise((resolve, reject) => {
+        console.log('Fazendo requisição...')
+        setTimeout(() => {
+            console.log('Enviando requisição')
+            resolve(config)
+        }, 2000)
+    })
+}, error => {
+    console.log('Erro ao fazer requisição: ', error)
+    return Promise.reject(error)
+})
+
+export default instance
